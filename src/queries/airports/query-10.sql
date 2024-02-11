84519009c2f4a2ce7f0d85581a2a50ccfa246ced
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-11 23:15:59 +0100

    finished query-10.sql

--- src/queries/airports/query-10.sql
+++ src/queries/airports/query-10.sql
@@ -1,4 +1,17 @@
 /* List the French cities from which more than two flights depart weekly directly to Italy. */
 
+SELECT
+    FLIGHTS.DepartureCityName AS "City"
+FROM
+    FLIGHTS
+    INNER JOIN CITIES AS DEPARTURE ON FLIGHTS.DepartureCityName = DEPARTURE.CityName
+    INNER JOIN CITIES AS ARRIVAL ON FLIGHTS.ArrivalCityName = ARRIVAL.CityName
+WHERE
+    DEPARTURE.Country = 'FRA' AND
+    ARRIVAL.Country = 'ITA'
+GROUP BY
+    FLIGHTS.DepartureCityName 
+HAVING
+    COUNT(*) > 2;
 
 
