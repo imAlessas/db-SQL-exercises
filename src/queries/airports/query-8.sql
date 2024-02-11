59bf5e5017a08495d26dd1e9a585368185b5fbc6
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-11 23:07:30 +0100

    fixed query-8.sql

--- src/queries/airports/query-8.sql
+++ src/queries/airports/query-8.sql
@@ -1,10 +1,12 @@
-/* Indicate the number of international flights departing on Monday from Venice. */
+/* Indicate the number of international flights departing on Monday from Manchester. */
 
 SELECT 
     COUNT(FLIGHTS.FlightId) AS "Number of flights"
 FROM 
     FLIGHTS
+    INNER JOIN CITIES AS ARRIVAL ON FLIGHTS.ArrivalCityName = ARRIVAL.CityName
 WHERE
-    FLIGHTS.DayOfWeek = 'LU' AND /* LU = Lunedi' which is Monday in italian*/
-    FLIGHTS.DepartureCityName = 'Venice';
+    ARRIVAL.Country <> 'GRE' AND
+    FLIGHTS.DayOfWeek = 'MO' AND
+    FLIGHTS.DepartureCityName = 'Manchester';
 
