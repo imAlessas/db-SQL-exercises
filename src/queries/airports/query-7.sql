f813069ebcf6540b5a2e8deeac88d7e25a867433
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-11 23:07:11 +0100

    minor changes

--- src/queries/airports/query-7.sql
+++ src/queries/airports/query-7.sql
@@ -1,11 +1,11 @@
-/* The cities from which there are direct flights to Dubai, ordered alphabetically. */
+/* The cities from which there are direct flights to Osaka, ordered alphabetically. */
 
 SELECT 
     FLIGHTS.DepartureCityName AS "Departure city"
 FROM 
     FLIGHTS
 WHERE 
-    FLIGHTS.ArrivalCityName = 'Dubai'
+    FLIGHTS.ArrivalCityName = 'Osaka'
 GROUP BY
     FLIGHTS.DepartureCityName
 ORDER BY 
