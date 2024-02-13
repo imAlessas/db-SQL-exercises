784fe2c6ecd455b4d7f9a0a95e660e76d330e5f6
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-13 11:36:48 +0100

    finished query-1.sql

--- src/queries/music/query-1.sql
+++ src/queries/music/query-1.sql
@@ -1,1 +1,18 @@
-/* The titles of the albums that contain at least one popular song. */
+/* The titles of the albums that contain at least one popular song.  A popular song is a song whose publication date is unknown */
+
+
+SELECT 
+    ALBUMS.AlbumTitle AS "Album"
+FROM
+    ALBUMS
+    INNER JOIN ALBUM_SONGS ON ALBUMS.AlbumId = ALBUM_SONGS.AlbumId
+WHERE
+    ALBUM_SONGS.SongId = (
+        SELECT
+            SongId
+        FROM
+            SONGS
+        WHERE
+            SONGS.SongYear IS NULL
+    );
+
